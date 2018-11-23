a950da8945c9b1c309ea798de4e08ba0dceee1ce
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-11-22 23:41:44 -0200

    stutter with chord progression

--- 02_testing_chords_in_key.py
+++ 02_testing_chords_in_key.py
@@ -1,10 +1,13 @@
 from FoxDot.lib import Chords
 
 ## Playing chords from a key
-p1 >> pluck([Chords.I, Chords.IV, Chords.II, Chords.IV], dur=4)
+# p1 >> pluck([Chords.I, Chords.IV, Chords.II, Chords.IV], dur=4)
+
+# Scale.default = 'minor'
+# p1 >> pluck([Chords.I, Chords.IV, Chords.II, Chords.IV], dur=4)
+
+# Scale.default = 'yu'
+# p1 >> pluck([Chords.I, Chords.IV, Chords.II, Chords.IV], dur=4)
 
 Scale.default = 'minor'
-p1 >> pluck([Chords.I, Chords.IV, Chords.II, Chords.IV], dur=4)
-
-Scale.default = 'yu'
-p1 >> pluck([Chords.I, Chords.IV, Chords.II, Chords.IV], dur=4)
+p1 >> pluck(P[Chords.I, Chords.IV, Chords.II, Chords.IV].stutter(2), dur=2)
