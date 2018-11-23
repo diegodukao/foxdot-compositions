f2d85a9215e47698e44bf76370e9230aac9b3483
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-11-22 23:46:53 -0200

    changing root of the scale and playing chord key degrees

--- 02_testing_chords_in_key.py
+++ 02_testing_chords_in_key.py
@@ -9,5 +9,10 @@ from FoxDot.lib import Chords
 # Scale.default = 'yu'
 # p1 >> pluck([Chords.I, Chords.IV, Chords.II, Chords.IV], dur=4)
 
-Scale.default = 'minor'
-p1 >> pluck(P[Chords.I, Chords.IV, Chords.II, Chords.IV].stutter(2), dur=2)
+# Scale.default = 'minor'
+# p1 >> pluck(P[Chords.I, Chords.IV, Chords.II, Chords.IV].stutter(2), dur=2)
+
+Scale.default = 'major'
+Root.default = 'D'
+# p1 >> pluck(P[Chords.VI, Chords.IV, Chords.I, Chords.V], dur=5)
+p1 >> pluck(P[Chords.VI, Chords.IV, Chords.I, Chords.V].stutter(2), dur=2)
