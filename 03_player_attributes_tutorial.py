4ba9bff6cd474d28f7c1b92d1ef08b7f458a5865
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-01-03 02:30:51 -0200

    more tests with player attributes

--- 03_player_attributes_tutorial.py
+++ 03_player_attributes_tutorial.py
@@ -15,3 +15,15 @@ p1 >> pluck([0, 2, 4, 6, 7], scale=Scale.dorian)
 ## amp
 # p1 >> pluck([0, 1, 2], dur=[1, 1/2, 1/2], amp=[1, 0.5, 1/3])
 p1 >> pluck(dur=1/4, amp=[1, 1/2, 1/2, 1, 0, 1, 0, 1, 1/2, 1/2, 1, 0, 1, 1/2, 1/4, 1])
+
+## samples
+# p1 >> play("x-o-")
+# p1 >> play("x-o-", sample=1)
+p1 >> play("x-o-", sample=[0, 1, 2])
+
+## delay
+# p1 >> pluck([0, 1, 2, 3], delay=[0, 0, 0.5])
+# "Stutter" every third note
+# p1 >> pluck([0, 1, 2], delay=[0, 0, (0, 0.5)])
+# Delay a note to play *after* the following one
+p1 >> pluck([0, 1, 2], delay=[0, 0, (0, 1.5)])
