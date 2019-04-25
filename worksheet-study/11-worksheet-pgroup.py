ee2785f09952b9fb969698602e36d9ccca7a523d
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-04-24 22:08:00 -0300

    adding worksheet study11

---
+++ worksheet-study/11-worksheet-pgroup.py
@@ -0,0 +1,43 @@
+# line 634
+
+# Along with playing notes together like so:
+    
+p1 >> pads(P(0, 2, 4, 6), dur=4)
+
+p1.stop()
+
+# PGroups can also be used to play notes in quick succession by putting
+# different symbols between the 'P' and the brackets:
+
+p1 >> pads(P(0, 2, 4, 6), dur=4)    
+
+p1 >> pads(P[0, 2, 4, 6], dur=4)
+
+p1 >> pads(P*(0, 2, 4, 6), dur=4)
+
+p1 >> pads(P[0, 2, 4, 6], dur=2)
+
+p1 >> pads(P*(0, 2, 4, 6), dur=2)
+
+
+p1.stop()
+
+# Notice the difference? The P* plays all the notes across the note's
+# duration - just like the square brackets in the "play" synth. You
+# can use the + symbol to spread the notes over a note's sustain
+# as opposed to the duration, which can be useful when using varying
+# lengths of durations:
+    
+p1 >> pluck(P*(0, 4), dur=PDur(3,8), sus=1)
+
+p1 >> pluck(P*(0, 4), dur=4, sus=2)
+
+p1 >> pluck(P+(0, 4), dur=PDur(3,8), sus=1)
+
+p1 >> pluck(P+(0, 4), dur=3, sus=1)
+
+p1 >> pluck(P*(0, 4, 6, 6, 2,), dur=3, sus=1)
+
+p1 >> pluck(P+(0, 4, 6, 6, 2,), dur=3, sus=1)
+
+p1 >> pluck(P+(0, 4), dur=PDur(3,8), sus=2)
