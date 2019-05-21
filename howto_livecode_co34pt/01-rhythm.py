edb09d8a6e26d7e11edbee91b002ecc3b541db57
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-05-21 14:29:32 -0300

    updating howtolivecode 01

--- howto_livecode_co34pt/01-rhythm.py
+++ howto_livecode_co34pt/01-rhythm.py
@@ -1,15 +1,12 @@
 # https://theseanco.github.io/howto_co34pt_liveCode/3-2-Basic-Rhythms/#preamble-how-to-construct-rhythms
 
-kd >> play("x", dur=1)
-
-sn >> play("o", dur=2)
-
+kd >> play("x", dur=1, amp=0.6)
+sn >> play("o", dur=2, amp=1)
 hh >> play("-", dur=1/4, amp=PWhite(0.25, 1))
-
 cl >> play("*", dur=0.75, amp=0.6)
+oh >> play("=", dur=P[0.5, 1])
 
-# not working
-# oh >> play("=", dur=Pseq([0.5, Pseq([1], inf)], inf))
+# why one Pseq inside another? how does it change the sound?
 # sc:
 #~oh = Pbind(\instrument,\bplay,\buf,d["oh"][0],\dur,Pseq([0.5,Pseq([1],inf)],inf),\amp,1);
 # ~oh.play;
