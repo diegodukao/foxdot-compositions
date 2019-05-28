0e1e711b2eea2840bd5f974044e7f95607ff49ec
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-05-28 19:33:42 -0300

    previous jam updated

--- references/qirky-03_chilled-vibes.py
+++ references/qirky-03_chilled-vibes.py
@@ -20,7 +20,8 @@ p3 >> blip(p1.pitch, dur=8, sus=4, room=1, oct=6) + [0,0,0,P*(2,4,3,-1)]
 p2 >> saw(P[:5][:9][:16], dur=1/4, oct=var([3,4],[12,4])).penta()
 print(P[:5][:9][:16])
 
-d1 >> play("(x )( x)o{ vx[xx]}", crush=16, rate=.8).every([24,5,3], "stutter", 4, dur=3)
+d1 >> play("(x )( x)o{ vx[xx]}", crush=16, rate=.8) \
+    .every([24,5,3], "stutter", 4, dur=3)
 
 d2 >> play("<-s>< ~*~>").every(30.5, "jump", cycle=32)
 
