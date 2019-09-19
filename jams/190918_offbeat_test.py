0a4332cfe5fe6b67eb8ed91c9d56f3e388555655
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-09-19 00:06:36 -0300

    jam updated

--- jams/190918_offbeat_test.py
+++ jams/190918_offbeat_test.py
@@ -1,6 +1,7 @@
 Scale.default = "minor"
 
-var.roots = var(0)
+# var.roots = var([0, 3, 2, 1], dur=[19, 6, 6, 6])
+var.roots = var([0], dur=[4])
 
 p1 >> keys(var.roots + (0, 2, 4), dur=[2], sus=1).offbeat(2)
 
