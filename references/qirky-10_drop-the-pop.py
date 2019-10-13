9f3774bdfbdc89e0269db3dd81887d457bca8949
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-10-13 19:46:13 -0300

    studying reference q10

--- references/qirky-10_drop-the-pop.py
+++ references/qirky-10_drop-the-pop.py
@@ -10,3 +10,19 @@ p1 >> pads((0,2,4,6), dur=16, amp=1/2, room=1, drive=0, shape=0.2, vib=12, slide
 p2 >> space([[0,2],4], dur=[6,2]).spread()
 p3 >> pulse([0,1,0,[1,2],0,4,5,4], lpf=linvar([500,2000],32), lpr=linvar([0.1,1],12), dur=1/2, amp=2*P[1,1,1,1,0,1,1,1]).spread().penta() + var([0,[1,2,3,-1]],[6,2])
 cr >> play("#", dur=32, room=1, amp=2).spread()
+
+b1.solo()
+
+p3.solo()
+
+Clock.bpm = 120
+# passing note
+p2 >> space([[0,2],4], dur=[6,2]).spread()
+
+p3 >> pulse([0,1,0,[1,2],0,4,5,4] + var([0,[1,2,3,-1]],[6,2]), lpf=linvar([500,2000],32),
+            lpr=linvar([0.1,1],12), dur=1/2,
+            amp=2*P[1,1,1,1,0,1,1,1]).spread().penta()
+
+p1 >> pads((0,2,4,6), dur=16, amp=1/2, room=1, drive=0, shape=0.2, vib=12, slide=1, slidedelay=0.5, chop=16, delay=0.5)
+
+b1 >> dbass(var([0,-2],8), dur=PDur(3,9), shape=PWhite(0,0.5), sus=2, chop=4, pan=PWhite(-1,1))
